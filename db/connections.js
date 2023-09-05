4f8b01438f4606e9524b543667269cd8d838b293
Mohamed Nasr <mhmdnsr.dev@gmail.com>
2023-09-05 23:57:38 +0300

    Edit URI of db connection

--- db/connections.js
+++ db/connections.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const initConnection = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://test-test:test-test@cluster0.3ewicv5.mongodb.net/user-post?retryWrites=true&w=majority"
+      "mongodb://localhost:27017/user-post"
     );
     console.log("connection done");
   } catch (error) {
